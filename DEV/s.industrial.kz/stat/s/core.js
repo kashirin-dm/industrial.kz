fb8de11d854a6118352ab2d1a8ced151e0080c09
Dmitriy <dim.fly.design@gmail.com>
2018-09-10 11:20:27 +0600

    Update menu grid

--- DEV/s.industrial.kz/stat/s/core.js
+++ DEV/s.industrial.kz/stat/s/core.js
@@ -1,19 +1,19 @@
 /* Функция ловит события скролла и передает прозрачность верхнему меню,
 когда позиция ниже 40px.
 Анимания происходит через CSS */
-function dsk_top_menu_opacity_bg() {
+function top_menu_opacity_bg() {
     if ($(window).scrollTop() <= 40) {
-        $('#dsk-top-menu').css('background', 'rgba(255,255,255,0)');
-        $('#dsk-top-menu').css('box-shadow', 'none');
+        $('#top-menu').css('background', 'rgba(255,255,255,0)');
+        $('#top-menu').css('box-shadow', 'none');
     }
     else {
-        $('#dsk-top-menu').css('background', 'rgba(255,255,255,1)');
-        $('#dsk-top-menu').css('box-shadow', 'rgba(0, 0, 0, 0.1) 0px 1px 3px');
-        }
+        $('#top-menu').css('background', 'rgba(255,255,255,1)');
+        $('#top-menu').css('box-shadow', 'rgba(0, 0, 0, 0.1) 0px 1px 3px');
+    }
 }
-dsk_top_menu_opacity_bg();
+top_menu_opacity_bg();
 $(window).scroll(function() {
-  dsk_top_menu_opacity_bg();
+    top_menu_opacity_bg();
 });
 
 
